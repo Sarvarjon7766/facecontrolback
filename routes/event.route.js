54e214f20a338eed7e73749496d859943ad731d2
Sarvarjon7766 <sarvaregamberdiyev758@gmail.com>
2025-08-14 09:08:20 +0500

    event route yangilandi

--- routes/event.route.js
+++ routes/event.route.js
@@ -2,6 +2,6 @@ const express = require('express')
 const router = express.Router()
 const { checkInAttandance } = require('../controllers/attandance.controller')
 
-router.post('/', checkInAttandance)
+router.post('/hik', checkInAttandance)
 
 module.exports = router
